6c7a9939223f8741f30a7d152b95c1c464c53bf5
shivankgoel003 <shivankgoel003@gmail.com>
2025-02-27 11:41:47 -0500

    EDA paper setup

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -16,33 +16,20 @@ library(dplyr)
 #### Clean data ####
 
 # Reading dataset
-breach_data <- read_excel("data/raw_data/Dataset-cyberattacks-06102020.xlsx", sheet = 2)
+df  <- read.csv("data/raw_data/STA305 Dataset.csv")
+df$name <- trimws(df$name)  # Removes extra spaces
+df$drug_dosage <- trimws(df$drug_dosage)  # Cleans up drug_dosage
 
-# Simplifying names
-breach_data <- clean_names(breach_data)
+sum(is.na(df$name))  # Check for missing names before split
 
 
-# Ensure 'attack_type' is treated as a character
-breach_data$attack_type <- as.character(breach_data$attack_type)
+# Separate the 'drug_dosage' column into 'drug' and 'dosage_level'
+df <- df %>%
+  separate(drug_dosage, into = c("drug", "dosage_level"), sep = "_") %>%
+  select(name, drug, dosage_level, everything())  # Reorder columns
 
-# Replace both NA values and "NA" strings with "Unknown" in 'attack_type' column
-breach_data <- breach_data %>%
-  mutate(attack_type = ifelse(is.na(attack_type) | attack_type == "NA", "Unknown", attack_type))
-
-# Replace both NA values and "NA" strings with "Unknown" in 'organisation_size' column
-breach_data <- breach_data %>%
-  mutate(organisation_size = ifelse(is.na(organisation_size) | organisation_size == "NA", "Unknown", organisation_size))
-breach_data <- breach_data %>%
-  filter(number_of_users_affected != "Missing")
-
-# Convert 'number_of_users_affected' to numeric, handling non-numeric cases
-breach_data$number_of_users_affected <- as.numeric(as.character(breach_data$number_of_users_affected))
-
-# Remove unwanted columns
-# Remove unwanted columns using dplyr::select to avoid any masking issues
-breach_data <- dplyr::select(breach_data, -c(summary, effect_on_share_price, inappropriate_remote_access))
-
+sum(is.na(df$name))  # Check for missing names before split
 
 # Saving cleaned data
-write.csv(breach_data, file = "data/analysis_data/breach_data.csv", row.names = FALSE)
-write_parquet(breach_data, "data/analysis_data/breach_data.parquet")
+write.csv(df, file = "data/analysis_data/clean_data.csv", row.names = FALSE)
+
