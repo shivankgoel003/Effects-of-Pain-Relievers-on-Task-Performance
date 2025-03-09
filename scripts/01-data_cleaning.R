172dfbfa5d5db06aa9caa7b4ab0fa64f805653a5
shivankgoel003 <shivankgoel003@gmail.com>
2025-03-09 12:43:00 -0400

    data set update

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -7,6 +7,7 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(tidyr)
 library(janitor)
 library(lubridate)
 library(readxl)
@@ -19,24 +20,30 @@ library(stringr)
 
 # Reading dataset
 df  <- read.csv("data/raw_data/STA305 Dataset.csv")
-df$name <- trimws(df$name)  # Removes extra spaces
-df$drug_dosage <- trimws(df$drug_dosage)  # Cleans up drug_dosage
-
-sum(is.na(df$name))  # Check for missing names before split
 
 
-# Separate the 'drug_dosage' column into 'drug' and 'dosage_level'
-df <- df %>%
-  separate(drug_dosage, into = c("drug", "dosage_level"), sep = "_") %>%
-  select(name, drug, dosage_level, everything())  # Reorder columns
+df_long_game <- df %>%
+  pivot_longer(
+    cols = c(game_before, game_after),
+    names_to = "time",
+    values_to = "game_score"
+  ) %>%
+  mutate(
+    # Convert "game_before" => "Before", "game_after" => "After"
+    time = ifelse(time == "game_before", "Before", "After")
+  )
 
 
+df_long_cards <- df %>%
+  pivot_longer(
+    cols = c(cards_before, cards_after),
+    names_to = "time",
+    values_to = "cards_score"
+  ) %>%
+  mutate(
+    time = ifelse(time == "cards_before", "Before", "After")
+  )
 
-# Convert first letter of categorical variables to uppercase
-df$drug <- str_to_title(df$drug)
-df$dosage_level <- str_to_title(df$dosage_level)
-df$location <- str_to_title(df$location)
-df$age_group <- str_to_title(df$age_group)
 
 # Saving cleaned data
 write.csv(df, file = "data/analysis_data/clean_data.csv", row.names = FALSE)
